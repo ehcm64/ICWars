16ed0a52b1ae1d0697725320659f573dc44da0f4
ehcm64 <92331699+ehcm64@users.noreply.github.com>
2021-12-06 00:05:58 +0100

    set hp pour soldat et tank

--- mp2-2021-provided/CS107-2021-ICWars/java/ch/epfl/cs107/play/game/icwars/actor/units/Tank.java
+++ mp2-2021-provided/CS107-2021-ICWars/java/ch/epfl/cs107/play/game/icwars/actor/units/Tank.java
@@ -7,12 +7,15 @@ import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.game.areagame.actor.Sprite;
 
 public class Tank extends Unit {
+    private float maxHp = 10;
+
 
     public Tank(Area area, DiscreteCoordinates position, Faction faction) {
         super(area, position, faction);
         this.moveRadius = 4;
         this.damage = 7;
         this.maxHp = 10;
+        this.hp = this.maxHp;
         if (faction.equals(Faction.ALLY)) spriteName = "icwars/friendlyTank";
         else spriteName = "icwars/enemyTank";
         sprite = new Sprite(spriteName, 1.5f, 1.5f, this, null, new Vector(-0.25f, -0.25f));
